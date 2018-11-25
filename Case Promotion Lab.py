e9dabd38e431a56ce869f61329957e6fbbf3bfb1
Chris H <churyn@splunk.com>
2018-11-25 19:07:43 +0000

    f

--- Case Promotion Lab.py
+++ Case Promotion Lab.py
@@ -75,7 +75,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
         conditions=[
-            ["artifact:*.cef.sourceDnsDomain", "==", ""],
+            ["artifact:*.cef.sourceDnsDomain", "!=", None],
         ],
         name="filter_1:condition_1")
 
