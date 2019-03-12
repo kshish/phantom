6cec26b64872731947458c12c86aba501917d197
admin <your-address@here.please>
2019-03-12 18:37:41 +0000

    asdf

--- lab 2 demo mar 12.py
+++ lab 2 demo mar 12.py
@@ -5,23 +5,36 @@ import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
 
+##############################
+# Start - Global Code Block
+
+import mymodule
+
+# End - Global Code block
+##############################
+
 def on_start(container):
     phantom.debug('on_start() called')
     
-    # call 'whois_domain_1' block
-    whois_domain_1(container=container)
+    # call 'Lookup_domain' block
+    Lookup_domain(container=container)
 
     return
 
-def whois_domain_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('whois_domain_1() called')
+"""
+This block looks up a domain from any artifacts that have destinationdnsdomain
 
-    # collect data for 'whois_domain_1' call
+
+"""
+def Lookup_domain(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('Lookup_domain() called')
+
+    # collect data for 'Lookup_domain' call
     container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationDnsDomain', 'artifact:*.id'])
 
     parameters = []
     
-    # build parameters list for 'whois_domain_1' call
+    # build parameters list for 'Lookup_domain' call
     for container_item in container_data:
         if container_item[0]:
             parameters.append({
@@ -30,7 +43,32 @@ def whois_domain_1(action=None, success=None, container=None, results=None, hand
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("whois domain", parameters=parameters, assets=['whois'], name="whois_domain_1")
+    phantom.act("whois domain", parameters=parameters, assets=['whois'], callback=send_warning_email, name="Lookup_domain")
+
+    return
+
+def send_warning_email(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('send_warning_email() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    phantom.act("send email", parameters=parameters, callback=format_1, name="send_warning_email", parent_action=action)
+
+    return
+
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('format_1() called')
+    
+    template = """{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "",
+    ]
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
 
     return
 
