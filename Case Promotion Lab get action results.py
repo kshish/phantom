dcf0aef0f1640f18aead0c1ed8590686cdce8a19
admin <your-address@here.please>
2019-03-17 03:54:41 +0000

    Saved: testing

--- Case Promotion Lab get action results.py
+++ Case Promotion Lab get action results.py
@@ -162,10 +162,9 @@ def no_op_1(action=None, success=None, container=None, results=None, handle=None
 
     phantom.act("no op", parameters=parameters, assets=['phantom extra actions'], callback=promote_to_case_1, name="no_op_1")
     
-    action_results = phantom.get_action_results(
-                        
-                        result_data=False, flatten=False)
-    phantom.debug(action_results)
+    results_data_1 = phantom.collect2(container=container, datapath=['geolocate_ip_1:action_result.message'], action_results=results)
+
+    phantom.debug(results_data_1)
 
     return
 
