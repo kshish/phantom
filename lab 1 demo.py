c4d1a964edb3bbf0151fc3910559b525563d75cf
admin <your-address@here.please>
2019-05-22 17:29:21 +0000

    testing wildcard in datapath

--- lab 1 demo.py
+++ lab 1 demo.py
@@ -82,7 +82,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 def add_comment_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     phantom.debug('add_comment_3() called')
 
-    results_data_1 = phantom.collect2(container=container, datapath=['geolocate_ip_1:*'], action_results=results)
+    results_data_1 = phantom.collect2(container=container, datapath=['geolocate_ip_1:action_result.data.*'], action_results=results)
 
     results_item_1_0 = [item[0] for item in results_data_1]
 
