10a43a4932e1602a3708c2d4cd832f7d9aaaea85
admin <your-address@here.please>
2019-06-26 16:35:51 +0000

    asdf

--- Lab 2 demo jun25.py
+++ Lab 2 demo jun25.py
@@ -153,6 +153,7 @@ def decision_2(action=None, success=None, container=None, results=None, handle=N
     # call connected blocks if condition 1 matched
     if matched_artifacts_1 or matched_results_1:
         playbook_local_Child_playbook_1(action=action, success=success, container=container, results=results, handle=handle)
+        playbook_chris_fish_for_phishing_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     return
@@ -162,6 +163,14 @@ def playbook_local_Child_playbook_1(action=None, success=None, container=None, r
     
     # call playbook "local/Child playbook", returns the playbook_run_id
     playbook_run_id = phantom.playbook("local/Child playbook", container=container)
+
+    return
+
+def playbook_chris_fish_for_phishing_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('playbook_chris_fish_for_phishing_1() called')
+    
+    # call playbook "chris/fish for phishing", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("chris/fish for phishing", container=container)
 
     return
 
