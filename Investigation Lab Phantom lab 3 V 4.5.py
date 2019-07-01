97ec2f6e72b499ecb8c5cd788248332535288c79
Chris Huryn <churyn@splunk.com>
2019-07-01 22:36:23 +0000

    filtered ips

--- Investigation Lab Phantom lab 3 V 4.5.py
+++ Investigation Lab Phantom lab 3 V 4.5.py
@@ -98,7 +98,7 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if matched_artifacts_1 or matched_results_1:
-        Notify_IT(action=action, success=success, container=container, results=results, handle=handle)
+        filter_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # call connected blocks for 'else' condition 2
@@ -125,7 +125,7 @@ def Notify_IT(action=None, success=None, container=None, results=None, handle=No
 
     # parameter list for template variable replacement
     parameters = [
-        "artifact:*.cef.destinationAddress",
+        "filtered-data:filter_1:condition_1:artifact:*.cef.destinationAddress",
     ]
 
     #responses:
@@ -143,6 +143,24 @@ def Notify_IT(action=None, success=None, container=None, results=None, handle=No
     ]
 
     phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="Notify_IT", parameters=parameters, response_types=response_types)
+
+    return
+
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_1() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["artifact:*.cef.destinationAddress", "!=", ""],
+        ],
+        name="filter_1:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        Notify_IT(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
