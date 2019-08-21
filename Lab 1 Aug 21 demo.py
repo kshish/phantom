c7aa1869a923cdc46c498a5e7cbc5bfc739f4570
admin <your-address@here.please>
2019-08-21 20:16:28 +0000

    Saved: asdf

--- Lab 1 Aug 21 demo.py
+++ Lab 1 Aug 21 demo.py
@@ -79,8 +79,8 @@ def Ask_analyst_to_set_severity_to_high(action=None, success=None, container=Non
     # parameter list for template variable replacement
     parameters = [
         "artifact:*.cef.sourceAddress",
-        "my_geolocate:action_result.data.*.country_name",
-        "my_geolocate:action_result.data.*.city_name",
+        "filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.country_name",
+        "filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.city_name",
     ]
 
     #responses:
