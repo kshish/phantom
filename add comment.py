c035944fd212db5905daa5966f2343ac6c1a8672
admin <your-address@here.please>
2019-10-30 14:39:06 +0000

    test

---
+++ add comment.py
@@ -0,0 +1,37 @@
+"""
+test to see order of container execution
+"""
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+def on_start(container):
+    phantom.debug('on_start() called')
+    
+    # call 'add_comment_1' block
+    add_comment_1(container=container)
+
+    return
+
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('add_comment_1() called')
+
+    phantom.comment(container=container, comment="chris wuz here")
+
+    return
+
+def on_finish(container, summary):
+    phantom.debug('on_finish() called')
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all detals of actions 
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    return
