557f796217cf7be17d5abaa12edb444be696da49
admin <root@localhost>
2020-02-13 21:27:52 +0000

    de

--- feb lab 2 demo.py
+++ feb lab 2 demo.py
@@ -59,7 +59,8 @@ def Ask_Analyst_to_set_high_severity(action=None, success=None, container=None, 
     user = "admin"
     message = """The ip {0} in the container {1}, {2} is not from U.S. It is from {3}
 
-Do you want to set container's severity to high?"""
+Do you want to set container's severity to high?
+{4}"""
 
     # parameter list for template variable replacement
     parameters = [
@@ -67,6 +68,7 @@ Do you want to set container's severity to high?"""
         "container:name",
         "container:description",
         "geolocate_ip_1:action_result.data.*.country_name",
+        "",
     ]
 
     #responses:
