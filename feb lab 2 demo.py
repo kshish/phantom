a4a3ae2dc16e913bb763fdf7398e1822b12eb40b
admin <root@localhost>
2020-02-13 19:55:40 +0000

    student manouver

--- feb lab 2 demo.py
+++ feb lab 2 demo.py
@@ -7,65 +7,6 @@ from datetime import datetime, timedelta
 
 def on_start(container):
     phantom.debug('on_start() called')
-    
-    # call 'geolocate_ip_1' block
-    geolocate_ip_1(container=container)
-
-    return
-
-def geolocate_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('geolocate_ip_1() called')
-
-    # collect data for 'geolocate_ip_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.sourceAddress', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'geolocate_ip_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'ip': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("geolocate ip", parameters=parameters, assets=['maxmind'], callback=decide_what_severity_to_set, name="geolocate_ip_1")
-
-    return
-
-def decide_what_severity_to_set(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('decide_what_severity_to_set() called')
-
-    # check for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["geolocate_ip_1:action_result.data.*.country_name", "!=", "United States"],
-        ])
-
-    # call connected blocks if condition 1 matched
-    if matched_artifacts_1 or matched_results_1:
-        Set_high_severity(action=action, success=success, container=container, results=results, handle=handle)
-        return
-
-    # call connected blocks for 'else' condition 2
-    set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
-
-    return
-
-def Set_high_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('Set_high_severity() called')
-
-    phantom.set_severity(container=container, severity="High")
-
-    return
-
-def set_low_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('set_low_severity() called')
-
-    phantom.set_severity(container=container, severity="Low")
 
     return
 
