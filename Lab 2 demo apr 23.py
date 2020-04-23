962de4192d3113c1f5dbe89b53b7a01908ca8b11
admin <root@localhost>
2020-04-23 20:00:20 +0000

    demo prompt

--- Lab 2 demo apr 23.py
+++ Lab 2 demo apr 23.py
@@ -5,14 +5,6 @@ This is a demo playbook
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-##############################
-# Start - Global Code Block
-
-import mymodule
-
-# End - Global Code block
-##############################
-
 def on_start(container):
     phantom.debug('on_start() called')
     
