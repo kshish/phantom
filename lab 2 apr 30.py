b9cdaa4108a2193dcf58d53327a7172e94bc4a18
admin <root@localhost>
2020-04-30 15:46:30 +0000

    demo debug

--- lab 2 apr 30.py
+++ lab 2 apr 30.py
@@ -4,23 +4,31 @@
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
+##############################
+# Start - Global Code Block
+
+import mymodule
+
+# End - Global Code block
+##############################
+
 def on_start(container):
     phantom.debug('on_start() called')
     
-    # call 'geolocate_ip_1' block
-    geolocate_ip_1(container=container)
+    # call 'my_better_geolocate' block
+    my_better_geolocate(container=container)
 
     return
 
-def geolocate_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('geolocate_ip_1() called')
+def my_better_geolocate(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('my_better_geolocate() called')
 
-    # collect data for 'geolocate_ip_1' call
+    # collect data for 'my_better_geolocate' call
     container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.sourceAddress', 'artifact:*.id'])
 
     parameters = []
     
-    # build parameters list for 'geolocate_ip_1' call
+    # build parameters list for 'my_better_geolocate' call
     for container_item in container_data:
         if container_item[0]:
             parameters.append({
@@ -28,8 +36,8 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': container_item[1]},
             })
-
-    phantom.act("geolocate ip", parameters=parameters, assets=['maxmind'], name="geolocate_ip_1")
+    phantom.debug(parameters)
+    phantom.act("geolocate ip", parameters=parameters, assets=['maxmind'], name="my_better_geolocate")
 
     return
 
