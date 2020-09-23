14c861678e9b0de386ba1463e10310e6cbb0116a
admin <YOUR@EMAIL>
2020-09-23 18:58:28 +0000

    demo smtp action

--- lab 2 demo sep 23 2020.py
+++ lab 2 demo sep 23 2020.py
@@ -30,7 +30,41 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], name="geolocate_ip_1")
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=send_email_1, name="geolocate_ip_1")
+
+    return
+
+def send_email_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('send_email_1() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    name_value = container.get('name', None)
+
+    # collect data for 'send_email_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.toEmail', 'artifact:*.id'])
+    results_data_1 = phantom.collect2(container=container, datapath=['geolocate_ip_1:action_result.data.*.country_name', 'geolocate_ip_1:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'send_email_1' call
+    for container_item in container_data:
+        for results_item_1 in results_data_1:
+            if container_item[0] and results_item_1[0]:
+                parameters.append({
+                    'from': "donotreply@splunk.com",
+                    'to': container_item[0],
+                    'cc': "",
+                    'bcc': "",
+                    'subject': name_value,
+                    'body': results_item_1[0],
+                    'attachments': "",
+                    'headers': "",
+                    # context (artifact id) is added to associate results with the artifact
+                    'context': {'artifact_id': container_item[1]},
+                })
+
+    phantom.act(action="send email", parameters=parameters, assets=['smtp'], name="send_email_1", parent_action=action)
 
     return
 
