bba8750396c9c4798de37e7b949e2e08e9ed5049
admin <YOUR@EMAIL>
2020-12-18 17:57:11 +0000

    demo

--- Working demo %%.py
+++ Working demo %%.py
@@ -5,7 +5,6 @@ This playbook will blah blah blah
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 def on_start(container):
     phantom.debug('on_start() called')
     
@@ -19,7 +18,7 @@ def on_start(container):
 """
 This code retrieves geographic  location information from an ip address
 """
-def my_geo_locate(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def my_geo_locate(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('my_geo_locate() called')
 
     # collect data for 'my_geo_locate' call
@@ -36,11 +35,11 @@ def my_geo_locate(action=None, success=None, container=None, results=None, handl
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("geolocate ip", parameters=parameters, assets=['maxmind'], callback=filter_1, name="my_geo_locate")
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=filter_1, name="my_geo_locate")
 
     return
 
-def Prompt_block_IP(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def Prompt_block_IP(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('Prompt_block_IP() called')
     
     # set user and message variables for phantom.prompt call
@@ -52,20 +51,25 @@ def Prompt_block_IP(action=None, success=None, container=None, results=None, han
         "format_2:formatted_data",
     ]
 
-    # response options
-    options = {
-        "type": "list",
-        "choices": [
-            "Yes",
-            "No",
-        ]
-    }
+    #responses:
+    response_types = [
+        {
+            "prompt": "",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No",
+                ]
+            },
+        },
+    ]
 
-    phantom.prompt(container=container, user=user, message=message, respond_in_mins=30, name="Prompt_block_IP", parameters=parameters, options=options, callback=decision_2)
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="Prompt_block_IP", parameters=parameters, response_types=response_types, callback=decision_2)
 
     return
 
-def decision_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def decision_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('decision_2() called')
 
     # check for 'if' condition 1
@@ -86,9 +90,9 @@ def decision_2(action=None, success=None, container=None, results=None, handle=N
 
     return
 
-def block_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def block_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('block_ip_1() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'block_ip_1' call
@@ -101,20 +105,20 @@ def block_ip_1(action=None, success=None, container=None, results=None, handle=N
         if inputs_item_1[0]:
             parameters.append({
                 'comment': "",
-                'direction': "asdf",
                 'protocol': "asdf",
-                'remote_port': "",
-                'ip_hostname': inputs_item_1[0],
+                'direction': "asdf",
                 'remote_ip': "",
+                'ip_hostname': inputs_item_1[0],
+                'remote_port': "",
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': inputs_item_1[1]},
             })
 
-    phantom.act("block ip", parameters=parameters, assets=['my local phantom'], callback=add_list_1, name="block_ip_1")
+    phantom.act(action="block ip", parameters=parameters, assets=['myloco'], callback=add_list_1, name="block_ip_1")
 
     return
 
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_1() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -128,14 +132,14 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        format_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        format_2(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
-def add_list_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def add_list_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('add_list_1() called')
 
-    filtered_results_data_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:my_geo_locate:action_result.data.1.country_name"])
+    filtered_results_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:my_geo_locate:action_result.data.1.country_name'])
 
     filtered_results_item_1_0 = [item[0] for item in filtered_results_data_1]
 
@@ -143,7 +147,7 @@ def add_list_1(action=None, success=None, container=None, results=None, handle=N
 
     return
 
-def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('format_2() called')
     
     template = """The following is the IP/Country geolocation information we looked up:
@@ -170,7 +174,7 @@ Country: {2}
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
