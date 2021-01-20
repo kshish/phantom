1ebaef9affd47a01b7cc35eac739831b1011fbe0
admin <root@localhost>
2021-01-20 06:21:19 +0000

    testing datapath with artifact numbers

---
+++ Wildcard in data path test - use numbers to reference first..second....py
@@ -0,0 +1,139 @@
+"""
+"""
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+def on_start(container):
+    phantom.debug('on_start() called')
+    
+    # call 'test_no_wildcard_use_first_artifact' block
+    test_no_wildcard_use_first_artifact(container=container)
+
+    # call 'test_with_second_artifact' block
+    test_with_second_artifact(container=container)
+
+    # call 'just_wildcard' block
+    just_wildcard(container=container)
+
+    return
+
+def test_no_wildcard_use_first_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('test_no_wildcard_use_first_artifact() called')
+
+    # collect data for 'test_no_wildcard_use_first_artifact' call
+
+    parameters = []
+    
+    # build parameters list for 'test_no_wildcard_use_first_artifact' call
+    parameters.append({
+        'ip': "artifact:0.cef.destinationAddress",
+    })
+
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=join_Datapath_results, name="test_no_wildcard_use_first_artifact")
+
+    return
+
+def Datapath_results(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('Datapath_results() called')
+    
+    # set user and message variables for phantom.prompt call
+    user = "admin"
+    message = """Specific path without wildcard:
+artifact:test.cef.destinationAddress
+ip {0}. country {1}
+----
+Wildcards surrounding word in datapath:
+artifact:*test*.cef.destinationAddress
+ip {2}. country {3}
+----
+Just wildcard (default)
+artifact:*.cef.destinationAddress
+ip {4}. country {5}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "test_no_wildcard_use_first_artifact:action_result.parameter.ip",
+        "test_no_wildcard_use_first_artifact:action_result.data.*.country_name",
+        "test_with_second_artifact:action_result.parameter.ip",
+        "test_with_second_artifact:action_result.data.*.country_name",
+        "just_wildcard:action_result.parameter.ip",
+        "just_wildcard:action_result.data.*.country_name",
+    ]
+
+    #responses:
+    response_types = [
+        {
+            "prompt": "",
+            "options": {
+                "type": "message",
+            },
+        },
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="Datapath_results", parameters=parameters, response_types=response_types)
+
+    return
+
+def join_Datapath_results(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
+    phantom.debug('join_Datapath_results() called')
+
+    # check if all connected incoming playbooks, actions, or custom functions are done i.e. have succeeded or failed
+    if phantom.completed(action_names=['test_no_wildcard_use_first_artifact', 'test_with_second_artifact', 'just_wildcard']):
+        
+        # call connected block "Datapath_results"
+        Datapath_results(container=container, handle=handle)
+    
+    return
+
+def test_with_second_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('test_with_second_artifact() called')
+
+    # collect data for 'test_with_second_artifact' call
+
+    parameters = []
+    
+    # build parameters list for 'test_with_second_artifact' call
+    parameters.append({
+        'ip': "artifact:1.cef.destinationAddress",
+    })
+
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=join_Datapath_results, name="test_with_second_artifact")
+
+    return
+
+def just_wildcard(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('just_wildcard() called')
+
+    # collect data for 'just_wildcard' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationAddress', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'just_wildcard' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'ip': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=join_Datapath_results, name="just_wildcard")
+
+    return
+
+def on_finish(container, summary):
+    phantom.debug('on_finish() called')
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all details of actions
+    # can be collected here.
+
+    summary_json = phantom.get_summary()
+    if 'result' in summary_json:
+        for action_result in summary_json['result']:
+            if 'action_run_id' in action_result:
+                action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                phantom.debug(action_results)
+
+    return
