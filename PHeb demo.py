cb325414546e599ec415be90a428210ba9e0781e
admin <root@localhost>
2021-02-03 17:14:14 +0000

    demo add artifact

--- PHeb demo.py
+++ PHeb demo.py
@@ -29,7 +29,37 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], name="geolocate_ip_1")
+    phantom.act(action="geolocate ip", parameters=parameters, assets=['maxmind'], callback=add_artifact_1, name="geolocate_ip_1")
+
+    return
+
+def add_artifact_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('add_artifact_1() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'add_artifact_1' call
+    results_data_1 = phantom.collect2(container=container, datapath=['geolocate_ip_1:action_result.data.*.country_name', 'geolocate_ip_1:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'add_artifact_1' call
+    for results_item_1 in results_data_1:
+        parameters.append({
+            'name': "User created artifact",
+            'container_id': "",
+            'label': "event",
+            'source_data_identifier': "chris",
+            'cef_name': "mySomeKindOfField",
+            'cef_value': results_item_1[0],
+            'cef_dictionary': "",
+            'contains': "",
+            'run_automation': "true",
+            # context (artifact id) is added to associate results with the artifact
+            'context': {'artifact_id': results_item_1[1]},
+        })
+
+    phantom.act(action="add artifact", parameters=parameters, assets=['mylocal'], name="add_artifact_1", parent_action=action)
 
     return
 
