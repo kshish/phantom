c5ed392cf81b0800b8c77897efb748c53e52b17b
Chris Huryn <root@localhost>
2021-12-14 17:00:54 +0000

    demo of list

---
+++ demo of list and prompt 50.py
@@ -0,0 +1,174 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'prompt_1' block
+    prompt_1(container=container)
+
+    return
+
+def prompt_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("prompt_1() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = "admin"
+    message = """Please enter in a color"""
+
+    # parameter list for template variable replacement
+    parameters = []
+
+    # responses
+    response_types = [
+        {
+            "prompt": "Color:",
+            "options": {
+                "type": "message",
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=1, name="prompt_1", parameters=parameters, response_types=response_types, callback=decision_1)
+
+    return
+
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["prompt_1:action_result.status", "==", "success"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        string_to_lowercase_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+def string_to_lowercase_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("string_to_lowercase_1() called")
+
+    prompt_1_result_data = phantom.collect2(container=container, datapath=["prompt_1:action_result.summary.responses.0","prompt_1:action_result.parameter.context.artifact_id"], action_results=results)
+
+    parameters = []
+
+    # build parameters list for 'string_to_lowercase_1' call
+    for prompt_1_result_item in prompt_1_result_data:
+        parameters.append({
+            "input_string": prompt_1_result_item[0],
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/string_to_lowercase", parameters=parameters, name="string_to_lowercase_1", callback=decision_2)
+
+    return
+
+
+def decision_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_2() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["string_to_lowercase_1:custom_function_result.data.lowercase_string", "in", "custom_list:mycolors"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        prompt_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    add_to_list_2(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+def prompt_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("prompt_2() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = "admin"
+    message = """Yes, the {0} color is in the list"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "string_to_lowercase_1:custom_function_result.data.lowercase_string"
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_2", parameters=parameters)
+
+    return
+
+
+def add_to_list_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_to_list_2() called")
+
+    string_to_lowercase_1__result = phantom.collect2(container=container, datapath=["string_to_lowercase_1:custom_function_result.data.lowercase_string"])
+
+    string_to_lowercase_1_data_lowercase_string = [item[0] for item in string_to_lowercase_1__result]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.add_list(list_name="mycolors", values=string_to_lowercase_1_data_lowercase_string)
+
+    return
+
+
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all details of actions
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
