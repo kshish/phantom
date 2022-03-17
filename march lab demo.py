fd004d70330c7a6c3fbcbf62b88abddd1ff79fc5
admin <root@localhost>
2022-03-17 17:06:28 +0000

    demo decision and prompt

--- march lab demo.py
+++ march lab demo.py
@@ -52,7 +52,7 @@ def mygeo_locate(action=None, success=None, container=None, results=None, handle
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="mygeo_locate", assets=["maxmind"], callback=debug_1)
+    phantom.act("geolocate ip", parameters=parameters, name="mygeo_locate", assets=["maxmind"], callback=decision_2)
 
     return
 
@@ -63,11 +63,12 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
     name_value = container.get("name", None)
     label_value = container.get("label", None)
     container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
-    mygeo_locate_result_data = phantom.collect2(container=container, datapath=["mygeo_locate:action_result.data.*.country_name","mygeo_locate:action_result.data.*.city_name","mygeo_locate:action_result.parameter.context.artifact_id"], action_results=results)
+    mygeo_locate_result_data = phantom.collect2(container=container, datapath=["mygeo_locate:action_result.data.*.country_name","mygeo_locate:action_result.data.*.city_name","mygeo_locate:action_result.data","mygeo_locate:action_result.parameter.context.artifact_id"], action_results=results)
 
     container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
     mygeo_locate_result_item_0 = [item[0] for item in mygeo_locate_result_data]
     mygeo_locate_result_item_1 = [item[1] for item in mygeo_locate_result_data]
+    mygeo_locate_result_item_2 = [item[2] for item in mygeo_locate_result_data]
 
     parameters = []
 
@@ -77,7 +78,7 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
         "input_3": label_value,
         "input_4": mygeo_locate_result_item_0,
         "input_5": mygeo_locate_result_item_1,
-        "input_6": None,
+        "input_6": mygeo_locate_result_item_2,
         "input_7": None,
         "input_8": None,
         "input_9": None,
@@ -95,6 +96,118 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
+
+    return
+
+
+def decision_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_2() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        logical_operator="or",
+        conditions=[
+            ["mygeo_locate:action_result.data.*.country_name", "==", "United States"],
+            ["mygeo_locate:action_result.data.*.country_name", "==", "Canada"],
+            ["mygeo_locate:action_result.data.*.country_name", "==", "Mexico"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        prompt_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    ask_for_high_severity(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+def prompt_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("prompt_1() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = "Administrator"
+    message = """The IPs are from USA, Canada, or Mexico"""
+
+    # parameter list for template variable replacement
+    parameters = []
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters)
+
+    return
+
+
+def ask_for_high_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("ask_for_high_severity() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = "Administrator"
+    message = """The container {0} is suspect!\n\nThe ip: {1} is from {2}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "container:name",
+        "mygeo_locate:action_result.parameter.ip",
+        "mygeo_locate:action_result.data.*.country_name"
+    ]
+
+    # responses
+    response_types = [
+        {
+            "prompt": "Would you like to change severity to High?",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No"
+                ],
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=1, name="ask_for_high_severity", parameters=parameters, response_types=response_types, callback=decision_3)
+
+    return
+
+
+def decision_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_3() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["ask_for_high_severity:action_result.summary.responses.0", "==", "Yes"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+def set_severity_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_severity_2() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.set_severity(container=container, severity="high")
+
+    container = phantom.get_container(container.get('id', None))
 
     return
 
