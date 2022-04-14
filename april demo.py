4b2781ab562da22d12e18008075c8f98857c9697
chris huryn <root@localhost>
2022-04-14 17:09:40 +0000

    demo utility function

--- april demo.py
+++ april demo.py
@@ -11,8 +11,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'my_geolocate' block
-    my_geolocate(container=container)
+    # call 'list_merge_4' block
+    list_merge_4(container=container)
 
     return
 
@@ -25,16 +25,15 @@ def my_geolocate(action=None, success=None, container=None, results=None, handle
     # This block gets geo locate info on an IP
     ################################################################################
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    list_merge_4_data = phantom.collect2(container=container, datapath=["list_merge_4:custom_function_result.data.*.item"])
 
     parameters = []
 
     # build parameters list for 'my_geolocate' call
-    for container_artifact_item in container_artifact_data:
-        if container_artifact_item[0] is not None:
+    for list_merge_4_data_item in list_merge_4_data:
+        if list_merge_4_data_item[0] is not None:
             parameters.append({
-                "ip": container_artifact_item[0],
-                "context": {'artifact_id': container_artifact_item[1]},
+                "ip": list_merge_4_data_item[0],
             })
 
     ################################################################################
@@ -93,6 +92,8 @@ def set_severity_1(action=None, success=None, container=None, results=None, hand
 
     container = phantom.get_container(container.get('id', None))
 
+    call_api_3(container=container)
+
     return
 
 
@@ -144,6 +145,7 @@ def evaluate_change_severity(action=None, success=None, container=None, results=
     # call connected blocks if condition 1 matched
     if found_match_1:
         set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+        geolocate_ip_2(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     return
@@ -183,6 +185,83 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         decide_if_ip_is_in_usa(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+def geolocate_ip_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("geolocate_ip_2() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_2", assets=["maxmind"])
+
+    return
+
+
+def call_api_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("call_api_3() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
+
+
+def list_merge_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("list_merge_4() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.cef.sourceAddress","artifact:*.cef.deviceAddress","artifact:*.id"])
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    container_artifact_cef_item_1 = [item[1] for item in container_artifact_data]
+    container_artifact_cef_item_2 = [item[2] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": container_artifact_cef_item_0,
+        "input_2": container_artifact_cef_item_1,
+        "input_3": container_artifact_cef_item_2,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/list_merge", parameters=parameters, name="list_merge_4", callback=my_geolocate)
 
     return
 
