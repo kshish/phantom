8aa0f14f3ed2d41382fbe159eba003e7877830fa
admin <root@localhost>
2022-04-21 19:57:19 +0000

    lab 3 task 2

--- Investigation Lab 5.3.py
+++ Investigation Lab 5.3.py
@@ -43,13 +43,13 @@ def locate_source(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="locate_source", assets=["maxmind"], callback=domain_reputation_1)
+    phantom.act("geolocate ip", parameters=parameters, name="locate_source", assets=["maxmind"], callback=source_reputation)
 
     return
 
 
-def domain_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("domain_reputation_1() called")
+def source_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("source_reputation() called")
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
@@ -57,7 +57,7 @@ def domain_reputation_1(action=None, success=None, container=None, results=None,
 
     parameters = []
 
-    # build parameters list for 'domain_reputation_1' call
+    # build parameters list for 'source_reputation' call
     for container_artifact_item in container_artifact_data:
         if container_artifact_item[0] is not None:
             parameters.append({
@@ -75,7 +75,80 @@ def domain_reputation_1(action=None, success=None, container=None, results=None,
     ## Custom Code End
     ################################################################################
 
-    phantom.act("domain reputation", parameters=parameters, name="domain_reputation_1", assets=["myvt"])
+    phantom.act("domain reputation", parameters=parameters, name="source_reputation", assets=["myvt"], callback=virus_search)
+
+    return
+
+
+def virus_search(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("virus_search() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.fileHash","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'virus_search' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "hash": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("file reputation", parameters=parameters, name="virus_search", assets=["myvt"], callback=debug_2)
+
+    return
+
+
+def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("debug_2() called")
+
+    locate_source_result_data = phantom.collect2(container=container, datapath=["locate_source:action_result.data.*.country_name","locate_source:action_result.parameter.context.artifact_id"], action_results=results)
+    virus_search_result_data = phantom.collect2(container=container, datapath=["virus_search:action_result.summary","virus_search:action_result.parameter.context.artifact_id"], action_results=results)
+    source_reputation_result_data = phantom.collect2(container=container, datapath=["source_reputation:action_result.summary","source_reputation:action_result.parameter.context.artifact_id"], action_results=results)
+
+    locate_source_result_item_0 = [item[0] for item in locate_source_result_data]
+    virus_search_result_item_0 = [item[0] for item in virus_search_result_data]
+    source_reputation_result_item_0 = [item[0] for item in source_reputation_result_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": locate_source_result_item_0,
+        "input_2": virus_search_result_item_0,
+        "input_3": source_reputation_result_item_0,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
 
     return
 
