14b73cbb9ac5be74818bc969f2d988c8b4d4ca9e
admin <root@localhost>
2022-04-22 03:58:04 +0000

    Lab 5 Task 1

--- Case Promotion Lab.py
+++ Case Promotion Lab.py
@@ -1,150 +1,87 @@
 """
+
 """
+
 
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
+
+
 def on_start(container):
     phantom.debug('on_start() called')
-    
-    # call 'promote_to_case_1' block
-    promote_to_case_1(container=container)
+
+    # call 'compose_report' block
+    compose_report(container=container)
 
     return
 
-def promote_to_case_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('promote_to_case_1() called')
+def compose_report(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("compose_report() called")
 
-    phantom.promote(container=container, template="Data Breach")
-    fixed_sourceDNS(container=container)
-
-    return
-
-def fixed_sourceDNS(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('fixed_sourceDNS() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["artifact:*.cef.sourceDnsDomain", "!=", ""],
-        ],
-        name="fixed_sourceDNS:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        fixed_filepath(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-def fixed_filepath(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('fixed_filepath() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["artifact:*.cef.filePath", "!=", ""],
-        ],
-        name="fixed_filepath:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        fixed_address(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-def fixed_address(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('fixed_address() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["artifact:*.cef.destinationAddress", "!=", ""],
-        ],
-        name="fixed_address:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        fixed_reason(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('format_1() called')
-    
-    template = """A file has been detected that has been determined to be potentially malicious. A case has been opened. 
-Case link: {0}
-Event Name: 
-Description: {2}
-Source URL: {3}
-Target Server IP: {4}
-Suspicious File Path: {5}
-Approver says: {6}"""
+    template = """A file has been detected that has been determined to be potentially malicious. A case has been opened. \n\n-**Case link**: {0}\n-**Event Name**: {1}\n-**Description**: {2}\n-**Source URL**: {3}\n-**Target Server**: {4}\n-**Suspicious File Path**: {5}\n-**Reason for promotion**: {6}"""
 
     # parameter list for template variable replacement
     parameters = [
         "container:url",
         "container:name",
         "container:description",
-        "filtered-data:fixed_sourceDNS:condition_1:artifact:*.cef.sourceDnsDomain",
-        "filtered-data:fixed_address:condition_1:artifact:*.cef.destinationAddress",
-        "filtered-data:fixed_filepath:condition_1:artifact:*.cef.filePath",
-        "filtered-data:fixed_reason:condition_1:artifact:*.cef.reason",
+        "artifact:*.cef.sourceDnsDomain",
+        "artifact:*.cef.destinationAddress",
+        "artifact:*.cef.filePath",
+        "playbook_input:promotion_reason"
     ]
 
-    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
 
-    send_email_1(container=container)
+    # Write your custom code here...
 
-    return
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
-def send_email_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('send_email_1() called')
+    phantom.format(container=container, template=template, parameters=parameters, name="compose_report", drop_none=True)
 
-    # collect data for 'send_email_1' call
-    formatted_data_1 = phantom.get_format_data(name='format_1')
-
-    parameters = []
-    
-    # build parameters list for 'send_email_1' call
-    parameters.append({
-        'body': formatted_data_1,
-        'from': "edu-labserver@splunk.com",
-        'attachments': "",
-        'to': "bwalden@splunk.com",
-        'cc': "",
-        'bcc': "",
-        'headers': "",
-        'subject': "New Case Created",
-    })
-
-    phantom.act("send email", parameters=parameters, assets=['smtp'], name="send_email_1")
+    promote_to_case_add_comment_add_note_1(container=container)
 
     return
 
-def fixed_reason(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('fixed_reason() called')
 
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["artifact:*.cef.reason", "!=", ""],
-        ],
-        name="fixed_reason:condition_1")
+def promote_to_case_add_comment_add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("promote_to_case_add_comment_add_note_1() called")
 
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        format_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+    compose_report = phantom.get_format_data(name="compose_report")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.promote(container=container, template="Data Breach")
+    phantom.comment(container=container, comment="Promoted to case")
+    phantom.add_note(container=container, content=compose_report, note_format="markdown", note_type="general", title="Incident Report")
+
+    container = phantom.get_container(container.get('id', None))
 
     return
+
 
 def on_finish(container, summary):
-    phantom.debug('on_finish() called')
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
@@ -153,5 +90,9 @@ def on_finish(container, summary):
             # if 'action_run_id' in action_result:
                 # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
                 # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
