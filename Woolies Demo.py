5b10e24f5fcd37d02d8934e4a84c7a794fb77f46
Chris Huryn <root@localhost>
2022-05-03 03:01:53 +0000

    demo naming blocks

--- Woolies Demo.py
+++ Woolies Demo.py
@@ -111,17 +111,17 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+        set_severity_to_low(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
-    set_severity_3(action=action, success=success, container=container, results=results, handle=handle)
+    set_severity_to_high(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
 
-def set_severity_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("set_severity_2() called")
+def set_severity_to_low(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_severity_to_low() called")
 
     ################################################################################
     ## Custom Code Start
@@ -140,8 +140,8 @@ def set_severity_2(action=None, success=None, container=None, results=None, hand
     return
 
 
-def set_severity_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("set_severity_3() called")
+def set_severity_to_high(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_severity_to_high() called")
 
     ################################################################################
     ## Custom Code Start
