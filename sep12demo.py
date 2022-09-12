0a4372b0e0b4d779cc3459393ec18276ecf22d61
Chris Huryn <root@localhost>
2022-09-12 18:41:29 +0000

    uncomment end block code

--- sep12demo.py
+++ sep12demo.py
@@ -43,7 +43,44 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_1", assets=["maxmind"])
+    phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_1", assets=["maxmind"], callback=send_email_1)
+
+    return
+
+
+def send_email_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("send_email_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    name_value = container.get("name", None)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.cn1","artifact:*.id"])
+    geolocate_ip_1_result_data = phantom.collect2(container=container, datapath=["geolocate_ip_1:action_result.data.*.country_name","geolocate_ip_1:action_result.parameter.context.artifact_id"], action_results=results)
+
+    parameters = []
+
+    # build parameters list for 'send_email_1' call
+    for container_artifact_item in container_artifact_data:
+        for geolocate_ip_1_result_item in geolocate_ip_1_result_data:
+            if container_artifact_item[0] is not None and geolocate_ip_1_result_item[0] is not None:
+                parameters.append({
+                    "to": container_artifact_item[0],
+                    "body": geolocate_ip_1_result_item[0],
+                    "subject": name_value,
+                    "context": {'artifact_id': geolocate_ip_1_result_item[1]},
+                })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("send email", parameters=parameters, name="send_email_1", assets=["myfakeemailserver"])
 
     return
 
@@ -59,12 +96,12 @@ def on_finish(container, summary):
     # summary of all the action and/or all details of actions
     # can be collected here.
 
-    # summary_json = phantom.get_summary()
-    # if 'result' in summary_json:
-        # for action_result in summary_json['result']:
-            # if 'action_run_id' in action_result:
-                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
-                # phantom.debug(action_results)
+    summary_json = phantom.get_summary()
+    if 'result' in summary_json:
+        for action_result in summary_json['result']:
+            if 'action_run_id' in action_result:
+                action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                phantom.debug(action_results)
 
     ################################################################################
     ## Custom Code End
