6bfb9751307d02f0aec46bae110d542807bb8361
Chris Huryn <root@localhost>
2022-09-12 20:33:22 +0000

    fix prompt logic

--- sep12demo.py
+++ sep12demo.py
@@ -192,7 +192,7 @@ def decision_5(action=None, success=None, container=None, results=None, handle=N
     found_match_1 = phantom.decision(
         container=container,
         conditions=[
-            ["prompt_1:action_result.summary.responses.0", "!=", "Yes"]
+            ["prompt_1:action_result.summary.responses.0", "==", "No"]
         ])
 
     # call connected blocks if condition 1 matched
