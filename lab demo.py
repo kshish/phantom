a4b0e733a79472d3306716f7480e492a0b7b2382
Chris Huryn <root@localhost>
2023-05-31 17:01:52 +0000

    demo format block

--- lab demo.py
+++ lab demo.py
@@ -111,7 +111,6 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        prompt_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'elif' condition 2
@@ -131,6 +130,7 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # check for 'else' condition 3
     pin_3(action=action, success=success, container=container, results=results, handle=handle)
+    format_1(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -189,14 +189,13 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
 
     user = None
     role = "Administrator"
-    message = """Container {0} with severity {1} has one or more IP(s) outside of our list.\n\nIP: {2} is from {3}"""
+    message = """Container {0} with severity {1} has one or more IP(s) outside of our list.\n\n{2}"""
 
     # parameter list for template variable replacement
     parameters = [
         "container:name",
         "container:severity",
-        "filtered-data:filter_out_none:condition_1:my_geo_locate_1:action_result.parameter.ip",
-        "filtered-data:filter_out_none:condition_1:my_geo_locate_1:action_result.data.*.country_name"
+        "format_1:formatted_data"
     ]
 
     # responses
@@ -344,6 +343,35 @@ def filter_out_none(action=None, success=None, container=None, results=None, han
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_1() called")
+
+    template = """IP: {0} is from {1}\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_out_none:condition_1:my_geo_locate_1:action_result.parameter.ip",
+        "filtered-data:filter_out_none:condition_1:my_geo_locate_1:action_result.data.*.country_name"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    prompt_1(container=container)
 
     return
 
