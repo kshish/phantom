db12b61407635bdab55828183e3d31168c812a36
Chris Huryn <root@localhost>
2023-05-31 16:36:41 +0000

    add list_merge

--- lab demo.py
+++ lab demo.py
@@ -12,55 +12,10 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'my_geo_locate' block
-    my_geo_locate(container=container)
+    # call 'list_merge_8' block
+    list_merge_8(container=container)
 
     return
-
-@phantom.playbook_block()
-def my_geo_locate(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("my_geo_locate() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
-
-    parameters = []
-
-    # build parameters list for 'my_geo_locate' call
-    for container_artifact_item in container_artifact_data:
-        if container_artifact_item[0] is not None:
-            parameters.append({
-                "ip": container_artifact_item[0],
-                "context": {'artifact_id': container_artifact_item[1]},
-            })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.act("geolocate ip", parameters=parameters, name="my_geo_locate", assets=["maxmind"], callback=my_geo_locate_callback)
-
-    return
-
-
-@phantom.playbook_block()
-def my_geo_locate_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("my_geo_locate_callback() called")
-
-    
-    debug_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
-    decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
-
-
-    return
-
 
 @phantom.playbook_block()
 def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
@@ -149,7 +104,8 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         logical_operator="and",
         conditions=[
             ["my_geo_locate:action_result.data.*.country_name", "!=", "United States"],
-            ["my_geo_locate:action_result.data.*.country_name", "!=", "Canada"]
+            ["my_geo_locate:action_result.data.*.country_name", "!=", "Canada"],
+            ["my_geo_locate:action_result.data.*.country_name", "!=", "Mexico"]
         ],
         case_sensitive=False)
 
@@ -298,6 +254,77 @@ def set_severity_5(action=None, success=None, container=None, results=None, hand
     phantom.set_severity(container=container, severity="high")
 
     container = phantom.get_container(container.get('id', None))
+
+    return
+
+
+@phantom.playbook_block()
+def list_merge_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("list_merge_8() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.cef.sourceAddress","artifact:*.id"])
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    container_artifact_cef_item_1 = [item[1] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": container_artifact_cef_item_0,
+        "input_2": container_artifact_cef_item_1,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/list_merge", parameters=parameters, name="list_merge_8", callback=my_geo_locate_1)
+
+    return
+
+
+@phantom.playbook_block()
+def my_geo_locate_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("my_geo_locate_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    list_merge_8_data = phantom.collect2(container=container, datapath=["list_merge_8:custom_function_result.data.*.item"])
+
+    parameters = []
+
+    # build parameters list for 'my_geo_locate_1' call
+    for list_merge_8_data_item in list_merge_8_data:
+        if list_merge_8_data_item[0] is not None:
+            parameters.append({
+                "ip": list_merge_8_data_item[0],
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("geolocate ip", parameters=parameters, name="my_geo_locate_1", assets=["maxmind"], callback=decision_1)
 
     return
 
