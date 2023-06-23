88cd88a821f6deb7ed5b4386effc568a09fed6e3
Chris Huryn <soardev@splunk.com>
2023-06-23 16:56:31 +0000

    demo format

--- jun222023.py
+++ jun222023.py
@@ -110,11 +110,11 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        prompt_for_high_severity(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     severity_low(action=action, success=success, container=container, results=results, handle=handle)
+    format_1(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -148,14 +148,13 @@ def prompt_for_high_severity(action=None, success=None, container=None, results=
 
     user = None
     role = "Administrator"
-    message = """The container {0} with severity {1}.\n\nIP: {2} country: {3}"""
+    message = """The container {0} with severity {1}.\n\n{2}"""
 
     # parameter list for template variable replacement
     parameters = [
         "container:name",
         "container:severity",
-        "filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.parameter.ip",
-        "filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.data.*.country_name"
+        "format_1:formatted_data"
     ]
 
     # responses
@@ -234,6 +233,35 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_1() called")
+
+    template = """IP: {0} is from Country: {1}\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.parameter.ip",
+        "filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.data.*.country_name"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    prompt_for_high_severity(container=container)
 
     return
 
