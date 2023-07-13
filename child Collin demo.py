409c1ef8adee57b09ccb94b1435242a03ecce736
soardev <soardev>
2023-07-13 21:09:23 +0000

    demo child pb

---
+++ child Collin demo.py
@@ -0,0 +1,152 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'set_severity_set_sensitivity_1' block
+    set_severity_set_sensitivity_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def set_severity_set_sensitivity_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_severity_set_sensitivity_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.set_severity(container=container, severity="high")
+    phantom.set_sensitivity(container=container, sensitivity="red")
+
+    container = phantom.get_container(container.get('id', None))
+
+    add_comment_2(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_2() called")
+
+    playbook_input_reason = phantom.collect2(container=container, datapath=["playbook_input:reason"])
+
+    playbook_input_reason_values = [item[0] for item in playbook_input_reason]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment=playbook_input_reason_values)
+
+    format_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def prompt_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("prompt_1() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = None
+    role = "Administrator"
+    message = """{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "format_1:formatted_data"
+    ]
+
+    # responses
+    response_types = [
+        {
+            "prompt": "Please enter a response",
+            "options": {
+                "type": "message",
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types)
+
+    return
+
+
+@phantom.playbook_block()
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_1() called")
+
+    template = """%%\nIP: {0} is from {1}\n%\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "playbook_input:ip_list",
+        "playbook_input:country_list"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    prompt_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    prompt_1_result_data = phantom.collect2(container=container, datapath=["prompt_1:action_result.summary.responses.0"])
+
+    prompt_1_summary_responses_0 = [item[0] for item in prompt_1_result_data]
+
+    output = {
+        "response": prompt_1_summary_responses_0,
+    }
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.save_playbook_output_data(output=output)
+
+    return
