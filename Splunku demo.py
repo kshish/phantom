9e322f7c33566c9724977da7e4a1d461d1d72adf
Chris Huryn <churyn@splunk.com>
2023-07-17 22:21:39 +0000

    demo list

--- Splunku demo.py
+++ Splunku demo.py
@@ -109,11 +109,8 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
     # check for 'if' condition 1
     found_match_1 = phantom.decision(
         container=container,
-        logical_operator="and",
         conditions=[
-            ["filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "United States"],
-            ["filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "Canada"],
-            ["filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "Ireland"]
+            ["filtered-data:filter_1:condition_1:my_geolocate:action_result.data.*.country_name", "not in", "custom_list:countries"]
         ],
         delimiter=None)
 
@@ -147,8 +144,8 @@ def pin_set_severity_set_label_2(action=None, success=None, container=None, resu
     ################################################################################
 
     phantom.pin(container=container, data=my_geolocate_result_item_0, message="IP(s) are in our list", pin_style="blue", pin_type="card")
-    phantom.set_severity(container=container, severity="low")
     phantom.set_label(container=container, label="friendly ips")
+    phantom.set_severity(container=container, severity="low")
 
     container = phantom.get_container(container.get('id', None))
 
