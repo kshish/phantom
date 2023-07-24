a728313a7946364263cba140705a0aa3560198eb
Chris Huryn <churyn@splunk.com>
2023-07-24 19:43:16 +0000

    rename blocks

--- lab demo jul2023.py
+++ lab demo jul2023.py
@@ -123,7 +123,7 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+        set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
@@ -133,8 +133,8 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
 
 @phantom.playbook_block()
-def set_severity_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("set_severity_2() called")
+def set_low_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_low_severity() called")
 
     ################################################################################
     ## Custom Code Start
@@ -216,7 +216,7 @@ def decision_2(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        set_severity_4(action=action, success=success, container=container, results=results, handle=handle)
+        set_high_severity(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     return
@@ -263,8 +263,8 @@ def debug_3(action=None, success=None, container=None, results=None, handle=None
 
 
 @phantom.playbook_block()
-def set_severity_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("set_severity_4() called")
+def set_high_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_high_severity() called")
 
     ################################################################################
     ## Custom Code Start
