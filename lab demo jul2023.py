e53f3c6abf8472bbd43be9d0f2910dfb939c03a8
Chris Huryn <churyn@splunk.com>
2023-07-25 16:59:01 +0000

    demo magical %%

--- lab demo jul2023.py
+++ lab demo jul2023.py
@@ -344,7 +344,7 @@ def list_merge_5(action=None, success=None, container=None, results=None, handle
 def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("format_1() called")
 
-    template = """IP: {0} is from {1}\n"""
+    template = """%%\nIP: {0} is from {1}\n%%"""
 
     # parameter list for template variable replacement
     parameters = [
