f6a869891a716411150b2ba40a2c15c55141cb83
Chris Huryn <churyn@splunk.com>
2023-08-14 19:01:45 +0000

    demo debug util

--- aug142023 demo.py
+++ aug142023 demo.py
@@ -1,5 +1,5 @@
 """
-
+This is a demo of a playbook
 """
 
 
@@ -7,6 +7,16 @@ import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
 
+
+################################################################################
+## Global Custom Code Start
+################################################################################
+
+
+import mymodule
+################################################################################
+## Global Custom Code End
+################################################################################
 
 @phantom.playbook_block()
 def on_start(container):
@@ -45,7 +55,80 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_1", assets=["maxmind"])
+    phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_1", assets=["maxmind"], callback=execute_program_1)
+
+    return
+
+
+@phantom.playbook_block()
+def execute_program_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("execute_program_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    parameters.append({
+        "ip_hostname": "127.0.0.1",
+        "command": "ps",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("execute program", parameters=parameters, name="execute_program_1", assets=["myssh"], callback=debug_1)
+
+    return
+
+
+@phantom.playbook_block()
+def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("debug_1() called")
+
+    name_value = container.get("name", None)
+    label_value = container.get("label", None)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    geolocate_ip_1_result_data = phantom.collect2(container=container, datapath=["geolocate_ip_1:action_result.data.*.country_name","geolocate_ip_1:action_result.parameter.context.artifact_id"], action_results=results)
+    execute_program_1_result_data = phantom.collect2(container=container, datapath=["execute_program_1:action_result.summary.exit_status","execute_program_1:action_result.data.*.output","execute_program_1:action_result.parameter.context.artifact_id"], action_results=results)
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    geolocate_ip_1_result_item_0 = [item[0] for item in geolocate_ip_1_result_data]
+    execute_program_1_summary_exit_status = [item[0] for item in execute_program_1_result_data]
+    execute_program_1_result_item_1 = [item[1] for item in execute_program_1_result_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": name_value,
+        "input_2": label_value,
+        "input_3": container_artifact_cef_item_0,
+        "input_4": geolocate_ip_1_result_item_0,
+        "input_5": execute_program_1_summary_exit_status,
+        "input_6": execute_program_1_result_item_1,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
