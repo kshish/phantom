003be9efc794f19639df040f20bb70d87c2e21c0
Chris Huryn <churyn@splunk.com>
2023-10-31 16:52:51 +0000

    demo format

--- lab demo Oct.py
+++ lab demo Oct.py
@@ -129,11 +129,11 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        prompt_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+    format_ip_and_country_list(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -167,14 +167,13 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
 
     user = None
     role = "Administrator"
-    message = """The container {0} with severity {1} has IP(s) outside our list.\n\nIP: {2} is from {3}"""
+    message = """The container {0} with severity {1} has IP(s) outside our list.\n\n{2}"""
 
     # parameter list for template variable replacement
     parameters = [
         "container:name",
         "container:severity",
-        "filtered-data:filter_out_none:condition_1:my_geolocate:action_result.parameter.ip",
-        "filtered-data:filter_out_none:condition_1:my_geolocate:action_result.data.*.country_name"
+        "format_ip_and_country_list:formatted_data"
     ]
 
     # responses
@@ -293,6 +292,35 @@ def list_merge_4(action=None, success=None, container=None, results=None, handle
     ################################################################################
 
     phantom.custom_function(custom_function="community/list_merge", parameters=parameters, name="list_merge_4", callback=my_geolocate)
+
+    return
+
+
+@phantom.playbook_block()
+def format_ip_and_country_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_ip_and_country_list() called")
+
+    template = """IP: {0} is from {1}\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_out_none:condition_1:my_geolocate:action_result.parameter.ip",
+        "filtered-data:filter_out_none:condition_1:my_geolocate:action_result.data.*.country_name"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_ip_and_country_list")
+
+    prompt_1(container=container)
 
     return
 
