711e588b4a3687e5e1ec5f671809e3ddce2d507d
Chris Huryn <churyn@splunk.com>
2023-10-31 16:55:19 +0000

    demo magical %%

--- lab demo Oct.py
+++ lab demo Oct.py
@@ -300,7 +300,7 @@ def list_merge_4(action=None, success=None, container=None, results=None, handle
 def format_ip_and_country_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("format_ip_and_country_list() called")
 
-    template = """IP: {0} is from {1}\n"""
+    template = """%%\nIP: {0} is from {1}\n%%"""
 
     # parameter list for template variable replacement
     parameters = [
