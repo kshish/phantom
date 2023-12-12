0020341e7f272fa34b692358a8627862d5ee0d93
Chris Huryn <churyn@splunk.com>
2023-12-12 19:11:33 +0000

    dddd

--- dec 2023 demo.py
+++ dec 2023 demo.py
@@ -148,7 +148,7 @@ def change_label(action=None, success=None, container=None, results=None, handle
     ## Custom Code End
     ################################################################################
 
-    phantom.set_label(container=container, label="notable - cleartext")
+    phantom.set_label(container=container, label="lowsev")
 
     container = phantom.get_container(container.get('id', None))
 
@@ -362,17 +362,17 @@ def list_merge_4(action=None, success=None, container=None, results=None, handle
 def playbook_dec_2023_child_pb_demo_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("playbook_dec_2023_child_pb_demo_1() called")
 
-    prompt_for_high_severity_result_data = phantom.collect2(container=container, datapath=["prompt_for_high_severity:action_result.summary.responses.1"], action_results=results)
     filtered_result_0_data_filter_out_none = phantom.collect2(container=container, datapath=["filtered-data:filter_out_none:condition_1:my_geolocate:action_result.parameter.ip","filtered-data:filter_out_none:condition_1:my_geolocate:action_result.data.*.country_name"])
+    prompt_for_high_severity_result_data = phantom.collect2(container=container, datapath=["prompt_for_high_severity:action_result.summary.responses.1"], action_results=results)
 
-    prompt_for_high_severity_summary_responses_1 = [item[0] for item in prompt_for_high_severity_result_data]
     filtered_result_0_parameter_ip = [item[0] for item in filtered_result_0_data_filter_out_none]
     filtered_result_0_data___country_name = [item[1] for item in filtered_result_0_data_filter_out_none]
+    prompt_for_high_severity_summary_responses_1 = [item[0] for item in prompt_for_high_severity_result_data]
 
     inputs = {
-        "reason_for_high_severity": prompt_for_high_severity_summary_responses_1,
         "ips": filtered_result_0_parameter_ip,
         "countries": filtered_result_0_data___country_name,
+        "reason_for_high_severity": prompt_for_high_severity_summary_responses_1,
     }
 
     ################################################################################
