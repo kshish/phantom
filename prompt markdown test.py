51ebe992d9a80e2255de98f617ab6cf72efaf9bd
soardev <soardev>
2024-04-12 16:50:51 +0000

    t

--- prompt markdown test.py
+++ prompt markdown test.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'prompt_1' block
-    prompt_1(container=container)
+    # call 'format_1' block
+    format_1(container=container)
 
     return
 
@@ -25,12 +25,42 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
 
     user = None
     role = "Administrator"
-    message = """this is a line.  \n\nthis a second line\n  \n  \n\n  \nthird line"""
+    message = """this is a line.  \n\nthis a second line\n  \n  {0}\n\n  \nthird line"""
 
     # parameter list for template variable replacement
-    parameters = []
+    parameters = [
+        "format_1:formatted_data"
+    ]
 
     phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters)
+
+    return
+
+
+@phantom.playbook_block()
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_1() called")
+
+    template = """test {0}\n  \n  \ntest 2  \n\n\nt3"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "container:name"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    prompt_1(container=container)
 
     return
 
