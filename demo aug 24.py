243c15622de8da3f461af26585f9761e37802a50
soardev <soardev>
2024-08-09 19:11:08 +0000

    demo filter (multidirection)

--- demo aug 24.py
+++ demo aug 24.py
@@ -111,29 +111,6 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
 
 
 @phantom.playbook_block()
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("decision_1() called")
-
-    # check for 'if' condition 1
-    found_match_1 = phantom.decision(
-        container=container,
-        conditions=[
-            ["filtered-data:filter_out_none:condition_1:my_geo_locate:action_result.data.*.country_name", "not in", "custom_list:countries"]
-        ],
-        delimiter=None)
-
-    # call connected blocks if condition 1 matched
-    if found_match_1:
-        format_ip_and_country_list(action=action, success=success, container=container, results=results, handle=handle)
-        return
-
-    # check for 'else' condition 2
-    set_label_5(action=action, success=success, container=container, results=results, handle=handle)
-
-    return
-
-
-@phantom.playbook_block()
 def prompt_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("prompt_2() called")
 
@@ -210,7 +187,7 @@ def filter_out_none(action=None, success=None, container=None, results=None, han
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        in_or_out_of_list(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
@@ -264,8 +241,8 @@ def format_ip_and_country_list(action=None, success=None, container=None, result
 
     # parameter list for template variable replacement
     parameters = [
-        "filtered-data:filter_out_none:condition_1:my_geo_locate:action_result.parameter.ip",
-        "filtered-data:filter_out_none:condition_1:my_geo_locate:action_result.data.*.country_name"
+        "filtered-data:in_or_out_of_list:condition_2:my_geo_locate:action_result.parameter.ip",
+        "filtered-data:in_or_out_of_list:condition_2:my_geo_locate:action_result.data.*.country_name"
     ]
 
     ################################################################################
@@ -384,6 +361,65 @@ def set_label_5(action=None, success=None, container=None, results=None, handle=
     phantom.set_label(container=container, label="splunk")
 
     container = phantom.get_container(container.get('id', None))
+
+    pin_2(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def in_or_out_of_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("in_or_out_of_list() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["filtered-data:filter_out_none:condition_1:my_geo_locate:action_result.data.*.country_name", "in", "custom_list:countries"]
+        ],
+        name="in_or_out_of_list:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        set_label_5(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["filtered-data:filter_out_none:condition_1:my_geo_locate:action_result.data.*.country_name", "not in", "custom_list:countries"]
+        ],
+        name="in_or_out_of_list:condition_2",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        format_ip_and_country_list(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+
+@phantom.playbook_block()
+def pin_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("pin_2() called")
+
+    filtered_result_0_data_in_or_out_of_list = phantom.collect2(container=container, datapath=["filtered-data:in_or_out_of_list:condition_1:my_geo_locate:action_result.data.*.country_name","filtered-data:in_or_out_of_list:condition_1:my_geo_locate:action_result.parameter.ip"])
+
+    filtered_result_0_data___country_name = [item[0] for item in filtered_result_0_data_in_or_out_of_list]
+    filtered_result_0_parameter_ip = [item[1] for item in filtered_result_0_data_in_or_out_of_list]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.pin(container=container, data=filtered_result_0_data___country_name, message=filtered_result_0_parameter_ip, pin_style="blue", pin_type="card")
 
     return
 
