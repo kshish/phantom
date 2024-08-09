d65940b65cb68553104c2eac8eab7f8a0af860c2
soardev <soardev>
2024-08-09 18:12:52 +0000

    demo case promo

--- aug 24 child pb demo.py
+++ aug 24 child pb demo.py
@@ -121,6 +121,29 @@ def set_severity_3(action=None, success=None, container=None, results=None, hand
 
     container = phantom.get_container(container.get('id', None))
 
+    promote_to_case_4(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def promote_to_case_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("promote_to_case_4() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.promote(container=container, template="Data Breach")
+
+    container = phantom.get_container(container.get('id', None))
+
     return
 
 
