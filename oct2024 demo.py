ee28186bbe3a2ab7e3526aab4ccaabcf4cceaac8
soardev <soardev>
2024-10-10 18:38:44 +0000

    demo debug util block

--- oct2024 demo.py
+++ oct2024 demo.py
@@ -78,7 +78,52 @@ def my_whois(action=None, success=None, container=None, results=None, handle=Non
     ## Custom Code End
     ################################################################################
 
-    phantom.act("whois domain", parameters=parameters, name="my_whois", assets=["whois"])
+    phantom.act("whois domain", parameters=parameters, name="my_whois", assets=["whois"], callback=debug_1)
+
+    return
+
+
+@phantom.playbook_block()
+def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_1() called")
+
+    name_value = container.get("name", None)
+    label_value = container.get("label", None)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    my_geolocate_result_data = phantom.collect2(container=container, datapath=["my_geolocate:action_result.status","my_geolocate:action_result.data.*.country_name","my_geolocate:action_result.parameter.context.artifact_id"], action_results=results)
+    my_whois_result_data = phantom.collect2(container=container, datapath=["my_whois:action_result.status","my_whois:action_result.parameter.context.artifact_id"], action_results=results)
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    my_geolocate_result_item_0 = [item[0] for item in my_geolocate_result_data]
+    my_geolocate_result_item_1 = [item[1] for item in my_geolocate_result_data]
+    my_whois_result_item_0 = [item[0] for item in my_whois_result_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": name_value,
+        "input_2": label_value,
+        "input_3": container_artifact_cef_item_0,
+        "input_4": my_geolocate_result_item_0,
+        "input_5": my_geolocate_result_item_1,
+        "input_6": my_whois_result_item_0,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
