e271df2a270c82d213da4331ef682c13bff914af
soardev <soardev>
2024-10-31 16:07:50 +0000

    fix input mapping to geo action

--- nov 2024 lab demo.py
+++ nov 2024 lab demo.py
@@ -27,14 +27,17 @@ def my_geolocate(action=None, success=None, container=None, results=None, handle
     # this is a comment for this block
     ################################################################################
 
-    description_value = container.get("description", None)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
 
     parameters = []
 
-    if description_value is not None:
-        parameters.append({
-            "ip": description_value,
-        })
+    # build parameters list for 'my_geolocate' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "ip": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
 
     ################################################################################
     ## Custom Code Start
