92229cd02ee7ebbb2f742ed484d5f52e9ad88ce5
soardev <soardev>
2024-11-01 16:48:32 +0000

    add low sev

--- nov 2024 lab demo.py
+++ nov 2024 lab demo.py
@@ -422,6 +422,29 @@ def pin_2(action=None, success=None, container=None, results=None, handle=None, 
 
     phantom.pin(container=container, data=filtered_result_0_parameter_ip, message=filtered_result_0_data___country_name, pin_style="blue", pin_type="card")
 
+    set_label_3(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def set_label_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("set_label_3() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.set_label(container=container, label="lowsev")
+
+    container = phantom.get_container(container.get('id', None))
+
     return
 
 
