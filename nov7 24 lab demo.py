b76342485f19816fe1ee86cf275d1d2d670b9188
soardev <soardev>
2024-11-07 21:09:22 +0000

    change logic

--- nov7 24 lab demo.py
+++ nov7 24 lab demo.py
@@ -111,22 +111,22 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
     # check for 'if' condition 1
     found_match_1 = phantom.decision(
         container=container,
-        logical_operator="or",
+        logical_operator="and",
         conditions=[
-            ["my_geolocate:action_result.data.*.country_name", "==", "United States"],
-            ["my_geolocate:action_result.data.*.country_name", "==", "Canada"],
-            ["my_geolocate:action_result.data.*.country_name", "==", "Belgium"],
-            ["my_geolocate:action_result.data.*.country_name", "==", "Norway"]
+            ["my_geolocate:action_result.data.*.country_name", "!=", "United States"],
+            ["my_geolocate:action_result.data.*.country_name", "!=", "Canada"],
+            ["my_geolocate:action_result.data.*.country_name", "!=", "Belgium"],
+            ["my_geolocate:action_result.data.*.country_name", "!=", "Norway"]
         ],
         delimiter=None)
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
+        set_high_severity(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
-    set_high_severity(action=action, success=success, container=container, results=results, handle=handle)
+    set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
