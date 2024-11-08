576e4a07af5b291a9f18a94aea81fa9ea100a44d
soardev <soardev>
2024-11-08 18:03:30 +0000

    demo format with %% (for each)

--- nov7 24 lab demo.py
+++ nov7 24 lab demo.py
@@ -296,7 +296,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 def format_ip_and_country_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("format_ip_and_country_list() called")
 
-    template = """IP: {0} is from {1}\n"""
+    template = """%%\nIP: {0} is from {1}\n%%"""
 
     # parameter list for template variable replacement
     parameters = [
