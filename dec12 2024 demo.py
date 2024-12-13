8cee8931ba7f27c85ee0bc8a7400a12523eed27d
soardev <soardev>
2024-12-13 20:14:50 +0000

    use list for better maintenance

--- dec12 2024 demo.py
+++ dec12 2024 demo.py
@@ -112,13 +112,8 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
     # check for 'if' condition 1
     found_match_1 = phantom.decision(
         container=container,
-        logical_operator="and",
         conditions=[
-            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "United States"],
-            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "Thailand"],
-            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "Saudi Arabia"],
-            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "India"],
-            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "!=", "Poland"]
+            ["filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name", "not in", "custom_list:countries"]
         ],
         delimiter=None)
 
