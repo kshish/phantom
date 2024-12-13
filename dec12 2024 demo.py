283411c1c0c53498a751f4490ab364217ba591c1
soardev <soardev>
2024-12-13 18:05:01 +0000

    demo format

--- dec12 2024 demo.py
+++ dec12 2024 demo.py
@@ -124,11 +124,11 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        prompt_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     low_severity(action=action, success=success, container=container, results=results, handle=handle)
+    format_ip_and_country_list(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -172,8 +172,6 @@ def high_severity(action=None, success=None, container=None, results=None, handl
 
     container = phantom.get_container(container.get('id', None))
 
-    format_1(container=container)
-
     return
 
 
@@ -185,14 +183,13 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
 
     user = None
     role = "Administrator"
-    message = """IP(s) not all from our list.\nContainer: {0} with severity: {1}\n\nWith IP: {2} is from: {3}"""
+    message = """IP(s) not all from our list.\nContainer: {0} with severity: {1}\n\n{2}"""
 
     # parameter list for template variable replacement
     parameters = [
         "container:name",
         "container:severity",
-        "filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.parameter.ip",
-        "filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name"
+        "format_ip_and_country_list:formatted_data"
     ]
 
     # responses
@@ -230,32 +227,6 @@ def decision_2(action=None, success=None, container=None, results=None, handle=N
     if found_match_1:
         high_severity(action=action, success=success, container=container, results=results, handle=handle)
         return
-
-    return
-
-
-@phantom.playbook_block()
-def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("format_1() called")
-
-    template = """{0}\n"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "my_geolocate:action_result.data.*.csv.*.field1"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
 
     return
 
@@ -316,6 +287,35 @@ def filter_to_keep_rows_with_countries(action=None, success=None, container=None
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def format_ip_and_country_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_ip_and_country_list() called")
+
+    template = """IP: {0} is from: {1}\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.parameter.ip",
+        "filtered-data:filter_to_keep_rows_with_countries:condition_1:my_geolocate:action_result.data.*.country_name"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_ip_and_country_list")
+
+    prompt_1(container=container)
 
     return
 
