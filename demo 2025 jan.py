c6866863d5a7e43fbd15d706fcb10b021f1295d1
soardev <soardev>
2025-01-27 19:57:30 +0000

    demo debug util

--- demo 2025 jan.py
+++ demo 2025 jan.py
@@ -14,8 +14,6 @@ def on_start(container):
 
     # call 'my_geolocate' block
     my_geolocate(container=container)
-    # call 'lookup_domain_1' block
-    lookup_domain_1(container=container)
 
     return
 
@@ -47,28 +45,38 @@ def my_geolocate(action=None, success=None, container=None, results=None, handle
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="my_geolocate", assets=["maxmind"])
+    phantom.act("geolocate ip", parameters=parameters, name="my_geolocate", assets=["maxmind"], callback=debug_1)
 
     return
 
 
 @phantom.playbook_block()
-def lookup_domain_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("lookup_domain_1() called")
+def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_1() called")
 
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    name_value = container.get("name", None)
+    label_value = container.get("label", None)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    my_geolocate_result_data = phantom.collect2(container=container, datapath=["my_geolocate:action_result.status","my_geolocate:action_result.data.*.country_name","my_geolocate:action_result.parameter.context.artifact_id"], action_results=results)
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceDnsDomain","artifact:*.id"])
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    my_geolocate_result_item_0 = [item[0] for item in my_geolocate_result_data]
+    my_geolocate_result_item_1 = [item[1] for item in my_geolocate_result_data]
 
     parameters = []
 
-    # build parameters list for 'lookup_domain_1' call
-    for container_artifact_item in container_artifact_data:
-        if container_artifact_item[0] is not None:
-            parameters.append({
-                "domain": container_artifact_item[0],
-                "context": {'artifact_id': container_artifact_item[1]},
-            })
+    parameters.append({
+        "input_1": name_value,
+        "input_2": label_value,
+        "input_3": container_artifact_cef_item_0,
+        "input_4": my_geolocate_result_item_0,
+        "input_5": my_geolocate_result_item_1,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
 
     ################################################################################
     ## Custom Code Start
@@ -80,7 +88,7 @@ def lookup_domain_1(action=None, success=None, container=None, results=None, han
     ## Custom Code End
     ################################################################################
 
-    phantom.act("lookup domain", parameters=parameters, name="lookup_domain_1", assets=["google_dns"])
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
