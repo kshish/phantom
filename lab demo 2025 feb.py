24a3569db131e2ecb9ed5edfcc59b69a6862b9c6
soardev <soardev>
2025-02-18 19:01:46 +0000

    demo

--- lab demo 2025 feb.py
+++ lab demo 2025 feb.py
@@ -14,6 +14,8 @@ def on_start(container):
 
     # call 'geolocate_ip_1' block
     geolocate_ip_1(container=container)
+    # call 'lookup_ip_1' block
+    lookup_ip_1(container=container)
 
     return
 
@@ -46,6 +48,39 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
     ################################################################################
 
     phantom.act("geolocate ip", parameters=parameters, name="geolocate_ip_1", assets=["maxmind"])
+
+    return
+
+
+@phantom.playbook_block()
+def lookup_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("lookup_ip_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'lookup_ip_1' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "ip": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("lookup ip", parameters=parameters, name="lookup_ip_1", assets=["google_dns"])
 
     return
 
