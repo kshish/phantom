759de0003ca82315a25b162f27a3911c0d0f9be0
soardev <soardev>
2025-03-06 20:23:44 +0000

    for each

--- lab demo VITA.py
+++ lab demo VITA.py
@@ -290,7 +290,7 @@ def set_high_severity(action=None, success=None, container=None, results=None, h
 def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("format_1() called")
 
-    template = """IP: {0} is from: {1}\n"""
+    template = """%%\nIP: {0} is from: {1}\n%%"""
 
     # parameter list for template variable replacement
     parameters = [
