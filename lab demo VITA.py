832d13210880a036e2428f3543d858445ce49983
soardev <soardev>
2025-03-07 14:49:01 +0000

    demo

--- lab demo VITA.py
+++ lab demo VITA.py
@@ -186,7 +186,7 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
         }
     ]
 
-    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=1, name="prompt_1", parameters=parameters, response_types=response_types, callback=prompt_1_callback)
+    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=1, name="prompt_1", parameters=parameters, response_types=response_types, callback=prompt_1_callback, drop_none=False)
 
     return
 
