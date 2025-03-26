cd2a1b06bfffa66dc281d9df95abefc9d3b5725b
soardev <soardev>
2025-03-26 18:39:54 +0000

    demo utility block

--- lab demo mar26 2025.py
+++ lab demo mar26 2025.py
@@ -1,5 +1,5 @@
 """
-
+This will be a comment in the python script
 """
 
 
@@ -23,7 +23,7 @@ def my_geolocate_ip(action=None, success=None, container=None, results=None, han
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.bytesOut","artifact:*.id"])
 
     parameters = []
 
@@ -45,7 +45,51 @@ def my_geolocate_ip(action=None, success=None, container=None, results=None, han
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="my_geolocate_ip", assets=["maxmind"])
+    phantom.act("geolocate ip", parameters=parameters, name="my_geolocate_ip", assets=["maxmind"], callback=debug_1)
+
+    return
+
+
+@phantom.playbook_block()
+def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_1() called")
+
+    name_value = container.get("name", None)
+    label_value = container.get("label", None)
+    my_geolocate_ip_result_data = phantom.collect2(container=container, datapath=["my_geolocate_ip:action_result.data.*.country_name","my_geolocate_ip:action_result.parameter.ip","my_geolocate_ip:action_result.data.*.latitude","my_geolocate_ip:action_result.parameter.context.artifact_id"], action_results=results)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.deviceAddress","artifact:*.id"])
+
+    my_geolocate_ip_result_item_0 = [item[0] for item in my_geolocate_ip_result_data]
+    my_geolocate_ip_parameter_ip = [item[1] for item in my_geolocate_ip_result_data]
+    my_geolocate_ip_result_item_2 = [item[2] for item in my_geolocate_ip_result_data]
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": my_geolocate_ip_result_item_0,
+        "input_2": my_geolocate_ip_parameter_ip,
+        "input_3": my_geolocate_ip_result_item_2,
+        "input_4": container_artifact_cef_item_0,
+        "input_5": name_value,
+        "input_6": label_value,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
