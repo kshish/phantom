2c339dad3838ee559c2a535c45be8143ac822576
soar_local_admin <root@localhost>
2025-08-21 20:24:11 +0000

    fix decision connectors

--- demo lab aug21 2025.py
+++ demo lab aug21 2025.py
@@ -73,11 +73,11 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
+        prompt_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
-    prompt_1(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
