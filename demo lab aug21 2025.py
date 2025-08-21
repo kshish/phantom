f50182c656a59fdad06f2de344fa398a93383b12
soar_local_admin <root@localhost>
2025-08-21 20:25:39 +0000

    demo prompt timeout

--- demo lab aug21 2025.py
+++ demo lab aug21 2025.py
@@ -157,7 +157,7 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
         }
     ]
 
-    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types, callback=decision_3)
+    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=1, name="prompt_1", parameters=parameters, response_types=response_types, callback=decision_3)
 
     return
 
