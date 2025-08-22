855dc6e48ebda17d20c6aff4e3e45dc7e0793e70
soar_local_admin <root@localhost>
2025-08-22 19:49:46 +0000

    asdf

--- demo lab aug21 2025.py
+++ demo lab aug21 2025.py
@@ -467,10 +467,9 @@ def pin_3(action=None, success=None, container=None, results=None, handle=None, 
 def pin_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("pin_4() called")
 
-    filtered_result_0_data_filter_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_2:my_geolocate:action_result.parameter.ip","filtered-data:filter_1:condition_2:my_geolocate:action_result.data.*.country_name"])
+    filtered_result_0_data_filter_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_2:my_geolocate:action_result.parameter.ip"])
 
     filtered_result_0_parameter_ip = [item[0] for item in filtered_result_0_data_filter_1]
-    filtered_result_0_data___country_name = [item[1] for item in filtered_result_0_data_filter_1]
 
     ################################################################################
     ## Custom Code Start
@@ -482,7 +481,7 @@ def pin_4(action=None, success=None, container=None, results=None, handle=None, 
     ## Custom Code End
     ################################################################################
 
-    phantom.pin(container=container, data=filtered_result_0_parameter_ip, message=filtered_result_0_data___country_name, pin_style="grey", pin_type="card")
+    phantom.pin(container=container, data=filtered_result_0_parameter_ip, message="Internal IP", pin_style="grey", pin_type="card")
 
     return
 
