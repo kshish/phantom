11c5d20170cb6680a0a11ccce48610ca9360121d
soardev <soardev>
2025-09-18 17:14:31 +0000

    demo

--- demo sep 2025.py
+++ demo sep 2025.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'my_geolocate' block
-    my_geolocate(container=container)
+    # call 'list_merge_5' block
+    list_merge_5(container=container)
 
     return
 
@@ -23,20 +23,26 @@ def my_geolocate(action=None, success=None, container=None, results=None, handle
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
+    ip_formatted_string = phantom.format(
+        container=container,
+        template="""{0}\n""",
+        parameters=[
+            "list_merge_5:custom_function_result.data.item"
+        ])
+
     ################################################################################
     # this is a comment about this block
     ################################################################################
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    list_merge_5__result = phantom.collect2(container=container, datapath=["list_merge_5:custom_function_result.data.item"])
 
     parameters = []
 
     # build parameters list for 'my_geolocate' call
-    for container_artifact_item in container_artifact_data:
-        if container_artifact_item[0] is not None:
+    for list_merge_5__result_item in list_merge_5__result:
+        if ip_formatted_string is not None:
             parameters.append({
-                "ip": container_artifact_item[0],
-                "context": {'artifact_id': container_artifact_item[1]},
+                "ip": ip_formatted_string,
             })
 
     ################################################################################
@@ -104,10 +110,11 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
 
     name_value = container.get("name", None)
     label_value = container.get("label", None)
-    my_geolocate_result_data = phantom.collect2(container=container, datapath=["my_geolocate:action_result.data.*.country_name","my_geolocate:action_result.parameter.ip","my_geolocate:action_result.parameter.context.artifact_id"], action_results=results)
+    my_geolocate_result_data = phantom.collect2(container=container, datapath=["my_geolocate:action_result.data.*.country_name","my_geolocate:action_result.parameter.ip","my_geolocate:action_result.data","my_geolocate:action_result.parameter.context.artifact_id"], action_results=results)
 
     my_geolocate_result_item_0 = [item[0] for item in my_geolocate_result_data]
     my_geolocate_parameter_ip = [item[1] for item in my_geolocate_result_data]
+    my_geolocate_result_item_2 = [item[2] for item in my_geolocate_result_data]
 
     parameters = []
 
@@ -116,7 +123,7 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
         "input_2": name_value,
         "input_3": label_value,
         "input_4": my_geolocate_parameter_ip,
-        "input_5": None,
+        "input_5": my_geolocate_result_item_2,
         "input_6": None,
         "input_7": None,
         "input_8": None,
@@ -187,8 +194,8 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
 
     # set approver and message variables for phantom.prompt call
 
-    user = "soardev"
-    role = None
+    user = None
+    role = "Administrator"
     message = """The {0} with severity {1} has IP(s) outside of our list\n\nIP: {2} is from: {3}\n"""
 
     # parameter list for template variable replacement
@@ -289,6 +296,47 @@ def debug_4(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_4")
+
+    return
+
+
+@phantom.playbook_block()
+def list_merge_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("list_merge_5() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.cef.deviceAddress","artifact:*.cef.sourceAddress","artifact:*.cef.myOwn","artifact:*.id"])
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    container_artifact_cef_item_1 = [item[1] for item in container_artifact_data]
+    container_artifact_cef_item_2 = [item[2] for item in container_artifact_data]
+    container_artifact_cef_item_3 = [item[3] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": container_artifact_cef_item_0,
+        "input_2": container_artifact_cef_item_1,
+        "input_3": container_artifact_cef_item_2,
+        "input_4": container_artifact_cef_item_3,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/list_merge", parameters=parameters, name="list_merge_5", callback=my_geolocate)
 
     return
 
