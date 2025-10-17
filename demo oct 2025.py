ae78cced52bebc43618adc762b486ea99c58f924
soardev <soardev>
2025-10-17 18:23:49 +0000

    demo child pb

--- demo oct 2025.py
+++ demo oct 2025.py
@@ -397,17 +397,17 @@ def format_1(action=None, success=None, container=None, results=None, handle=Non
 def playbook_demo_oct_2025_child_pb_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("playbook_demo_oct_2025_child_pb_1() called")
 
-    prompt_1_result_data = phantom.collect2(container=container, datapath=["prompt_1:action_result.summary.responses.1"], action_results=results)
     filtered_result_0_data_filter_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:my_geolocate_blagh:action_result.data.*.country_name","filtered-data:filter_1:condition_1:my_geolocate_blagh:action_result.parameter.ip"])
+    prompt_1_result_data = phantom.collect2(container=container, datapath=["prompt_1:action_result.summary.responses.1"], action_results=results)
 
-    prompt_1_summary_responses_1 = [item[0] for item in prompt_1_result_data]
     filtered_result_0_data___country_name = [item[0] for item in filtered_result_0_data_filter_1]
     filtered_result_0_parameter_ip = [item[1] for item in filtered_result_0_data_filter_1]
+    prompt_1_summary_responses_1 = [item[0] for item in prompt_1_result_data]
 
     inputs = {
-        "reason_for_high_severity": prompt_1_summary_responses_1,
         "pin_msg": filtered_result_0_data___country_name,
         "pin_data": filtered_result_0_parameter_ip,
+        "reason_for_high_severity": prompt_1_summary_responses_1,
     }
 
     ################################################################################
@@ -482,7 +482,10 @@ def format_high_risk_score_msg(action=None, success=None, container=None, result
 def add_note_11(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("add_note_11() called")
 
+    playbook_demo_oct_2025_child_pb_1_output_notes = phantom.collect2(container=container, datapath=["playbook_demo_oct_2025_child_pb_1:playbook_output:notes"])
     format_high_risk_score_msg = phantom.get_format_data(name="format_high_risk_score_msg")
+
+    playbook_demo_oct_2025_child_pb_1_output_notes_values = [item[0] for item in playbook_demo_oct_2025_child_pb_1_output_notes]
 
     ################################################################################
     ## Custom Code Start
@@ -494,7 +497,7 @@ def add_note_11(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.add_note(container=container, note_format="markdown", note_type="general", title=format_high_risk_score_msg)
+    phantom.add_note(container=container, content=playbook_demo_oct_2025_child_pb_1_output_notes_values, note_format="markdown", note_type="general", title=format_high_risk_score_msg)
 
     return
 
