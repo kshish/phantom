ffcec7b8cf0b0c266f1dd2e8f75bbcd96e75f028
soardev <soardev>
2025-10-17 17:07:37 +0000

    fix decision path

--- demo oct 2025.py
+++ demo oct 2025.py
@@ -128,11 +128,11 @@ def decision_2(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
+        format_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     add_comment_5(action=action, success=success, container=container, results=results, handle=handle)
-    format_1(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
