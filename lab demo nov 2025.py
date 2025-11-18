9bd8ce96436d678aa9a751ed6f567d6ac2682194
soardev <soardev>
2025-11-18 19:30:12 +0000

    asdf

--- lab demo nov 2025.py
+++ lab demo nov 2025.py
@@ -94,14 +94,14 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         return
 
     # check for 'else' condition 2
-    set_low_severity(action=action, success=success, container=container, results=results, handle=handle)
+    set_label(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
 
 @phantom.playbook_block()
-def set_low_severity(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("set_low_severity() called")
+def set_label(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("set_label() called")
 
     ################################################################################
     ## Custom Code Start
@@ -113,7 +113,7 @@ def set_low_severity(action=None, success=None, container=None, results=None, ha
     ## Custom Code End
     ################################################################################
 
-    phantom.set_severity(container=container, severity="low")
+    phantom.set_label()
 
     container = phantom.get_container(container.get('id', None))
 
