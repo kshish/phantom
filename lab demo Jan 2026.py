f5ba0da008805e2deb8cc610fa3244f240569e05
soardev <soardev>
2026-01-15 20:37:54 +0000

    demo decision

--- lab demo Jan 2026.py
+++ lab demo Jan 2026.py
@@ -45,7 +45,19 @@ def my_geo_locate_ip(action=None, success=None, container=None, results=None, ha
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="my_geo_locate_ip", assets=["maxmind"], callback=debug_1)
+    phantom.act("geolocate ip", parameters=parameters, name="my_geo_locate_ip", assets=["maxmind"], callback=my_geo_locate_ip_callback)
+
+    return
+
+
+@phantom.playbook_block()
+def my_geo_locate_ip_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("my_geo_locate_ip_callback() called")
+
+    
+    debug_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+    decision_3(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+
 
     return
 
@@ -88,6 +100,84 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
+
+    return
+
+
+@phantom.playbook_block()
+def decision_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("decision_3() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        logical_operator="or",
+        conditions=[
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "US"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "DE"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "SA"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "CA"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "MX"]
+        ],
+        conditions_dps=[
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "US"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "DE"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "SA"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "CA"],
+            ["my_geo_locate_ip:action_result.data.*.country_iso_code", "==", "MX"]
+        ],
+        name="decision_3:condition_1",
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        set_severity_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    set_severity_3(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def set_severity_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("set_severity_2() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.set_severity(container=container, severity="low")
+
+    container = phantom.get_container(container.get('id', None))
+
+    return
+
+
+@phantom.playbook_block()
+def set_severity_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("set_severity_3() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.set_severity(container=container, severity="high")
+
+    container = phantom.get_container(container.get('id', None))
 
     return
 
