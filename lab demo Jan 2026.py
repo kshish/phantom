d8528fc850687478fa8bbfaa7c00585c1a2c130e
soardev <soardev>
2026-01-16 17:54:24 +0000

    demo list_merge

--- lab demo Jan 2026.py
+++ lab demo Jan 2026.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'my_geo_locate_ip' block
-    my_geo_locate_ip(container=container)
+    # call 'list_merge_6' block
+    list_merge_6(container=container)
 
     return
 
@@ -23,16 +23,15 @@ def my_geo_locate_ip(action=None, success=None, container=None, results=None, ha
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+    list_merge_6__result = phantom.collect2(container=container, datapath=["list_merge_6:custom_function_result.data.item"])
 
     parameters = []
 
     # build parameters list for 'my_geo_locate_ip' call
-    for container_artifact_item in container_artifact_data:
-        if container_artifact_item[0] is not None:
+    for list_merge_6__result_item in list_merge_6__result:
+        if list_merge_6__result_item[0] is not None:
             parameters.append({
-                "ip": container_artifact_item[0],
-                "context": {'artifact_id': container_artifact_item[1]},
+                "ip": list_merge_6__result_item[0],
             })
 
     ################################################################################
@@ -68,11 +67,12 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
 
     name_value = container.get("name", None)
     label_value = container.get("label", None)
-    my_geo_locate_ip_result_data = phantom.collect2(container=container, datapath=["my_geo_locate_ip:action_result.data.*.country_iso_code","my_geo_locate_ip:action_result.data.*.country_name","my_geo_locate_ip:action_result.parameter.ip","my_geo_locate_ip:action_result.parameter.context.artifact_id"], action_results=results)
+    my_geo_locate_ip_result_data = phantom.collect2(container=container, datapath=["my_geo_locate_ip:action_result.data.*.country_iso_code","my_geo_locate_ip:action_result.data.*.country_name","my_geo_locate_ip:action_result.parameter.ip","my_geo_locate_ip:action_result.data","my_geo_locate_ip:action_result.parameter.context.artifact_id"], action_results=results)
 
     my_geo_locate_ip_result_item_0 = [item[0] for item in my_geo_locate_ip_result_data]
     my_geo_locate_ip_result_item_1 = [item[1] for item in my_geo_locate_ip_result_data]
     my_geo_locate_ip_parameter_ip = [item[2] for item in my_geo_locate_ip_result_data]
+    my_geo_locate_ip_result_item_3 = [item[3] for item in my_geo_locate_ip_result_data]
 
     parameters = []
 
@@ -82,7 +82,7 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
         "input_3": name_value,
         "input_4": label_value,
         "input_5": my_geo_locate_ip_parameter_ip,
-        "input_6": None,
+        "input_6": my_geo_locate_ip_result_item_3,
         "input_7": None,
         "input_8": None,
         "input_9": None,
@@ -293,6 +293,46 @@ def debug_4(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_4")
+
+    return
+
+
+@phantom.playbook_block()
+def list_merge_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("list_merge_6() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.cef.deviceAddress","artifact:*.cef.sourceAddress","artifact:*.id"])
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+    container_artifact_cef_item_1 = [item[1] for item in container_artifact_data]
+    container_artifact_cef_item_2 = [item[2] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": container_artifact_cef_item_0,
+        "input_2": container_artifact_cef_item_1,
+        "input_3": container_artifact_cef_item_2,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/list_merge", parameters=parameters, name="list_merge_6", callback=my_geo_locate_ip)
 
     return
 
