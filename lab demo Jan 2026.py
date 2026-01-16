8fcdffd969825e6d86b5b3632c8de69141c98605
soardev <soardev>
2026-01-16 18:12:38 +0000

    fix deconnection

--- lab demo Jan 2026.py
+++ lab demo Jan 2026.py
@@ -131,11 +131,11 @@ def decision_3(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if found_match_1:
+        format_3(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     set_sev_low(action=action, success=success, container=container, results=results, handle=handle)
-    format_3(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
