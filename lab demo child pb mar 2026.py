89c0856fa4a417426dce6712c928038177555246
soardev <soardev>
2026-03-19 18:41:52 +0000

    also promote

--- lab demo child pb mar 2026.py
+++ lab demo child pb mar 2026.py
@@ -14,6 +14,8 @@ def on_start(container):
 
     # call 'set_severity_1' block
     set_severity_1(container=container)
+    # call 'promote_to_case_4' block
+    promote_to_case_4(container=container)
 
     return
 
@@ -121,6 +123,27 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
     ]
 
     phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types)
+
+    return
+
+
+@phantom.playbook_block()
+def promote_to_case_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("promote_to_case_4() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.promote(container=container, template="Risk Investigation")
+
+    container = phantom.get_container(container.get('id', None))
 
     return
 
