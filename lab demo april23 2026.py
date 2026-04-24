67b805011475d5d39e9a7e4a962bcc66c5dc4a99
soardev <soardev>
2026-04-24 17:19:02 +0000

    pin demo

--- lab demo april23 2026.py
+++ lab demo april23 2026.py
@@ -153,6 +153,8 @@ def set_low_severity(action=None, success=None, container=None, results=None, ha
 
     container = phantom.get_container(container.get('id', None))
 
+    pin_9(container=container)
+
     return
 
 
@@ -173,6 +175,8 @@ def set_high_severity(action=None, success=None, container=None, results=None, h
     phantom.set_severity(container=container, severity="high")
 
     container = phantom.get_container(container.get('id', None))
+
+    pin_8(container=container)
 
     return
 
@@ -434,6 +438,56 @@ def format_2(action=None, success=None, container=None, results=None, handle=Non
     phantom.format(container=container, template=template, parameters=parameters, name="format_2")
 
     prompt_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def pin_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("pin_8() called")
+
+    filtered_result_0_data_filter_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.data.*.country_name","filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.parameter.ip"])
+
+    filtered_result_0_data___country_name = [item[0] for item in filtered_result_0_data_filter_1]
+    filtered_result_0_parameter_ip = [item[1] for item in filtered_result_0_data_filter_1]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.pin(container=container, data=filtered_result_0_data___country_name, message=filtered_result_0_parameter_ip, pin_style="red", pin_type="card")
+
+    container = phantom.get_container(container.get('id', None))
+
+    return
+
+
+@phantom.playbook_block()
+def pin_9(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("pin_9() called")
+
+    filtered_result_0_data_filter_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.data.*.country_name","filtered-data:filter_1:condition_1:geolocate_ip_1:action_result.parameter.ip"])
+
+    filtered_result_0_data___country_name = [item[0] for item in filtered_result_0_data_filter_1]
+    filtered_result_0_parameter_ip = [item[1] for item in filtered_result_0_data_filter_1]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.pin(container=container, data=filtered_result_0_data___country_name, message=filtered_result_0_parameter_ip, pin_style="blue", pin_type="card")
 
     return
 
